26753aa8fdc4c335312c71988dba6953bb86deab
Gustavo Carvalho <99373133+gustavosett@users.noreply.github.com>
2025-02-21 17:34:00 -0300

    feat: add new router

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,11 +1,20 @@
 from fastapi import APIRouter
 
-from app.api.routes import checkout, configs, login, subscription_plans, users, utils
+from app.api.routes import (
+    checkout,
+    configs,
+    login,
+    payments,
+    subscription_plans,
+    users,
+    utils,
+)
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
+api_router.include_router(payments.router, prefix="/payments", tags=["payments"])
 api_router.include_router(configs.router, prefix="/configs", tags=["configs"])
 api_router.include_router(
     subscription_plans.router, prefix="/subscription-plans", tags=["subscription-plans"]
