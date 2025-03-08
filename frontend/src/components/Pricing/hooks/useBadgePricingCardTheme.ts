896be4df5f73a16cf205cdc556fb1ee3542a629b
Gustavo Carvalho <99373133+gustavosett@users.noreply.github.com>
2025-03-07 23:51:14 -0300

    chore: improve badge color

--- frontend/src/components/Pricing/hooks/useBadgePricingCardTheme.ts
+++ frontend/src/components/Pricing/hooks/useBadgePricingCardTheme.ts
@@ -9,22 +9,22 @@ export const useBadgePricingCardTheme = (isLandingPage: boolean) => {
     : useColorModeValue("gray.700", "white")
   const borderColor = isLandingPage
     ? "teal.100"
-    : useColorModeValue("teal.100", "teal.800")
+    : useColorModeValue("#99D5CF", "teal.900")
   const priceColor = isLandingPage
-    ? "teal.700"
-    : useColorModeValue("teal.700", "teal.200")
+    ? "ui.main"
+    : useColorModeValue("ui.main", "ui.main")
   const discountColor = isLandingPage
     ? "gray.400"
     : useColorModeValue("gray.400", "gray.500")
   const highlightColor = isLandingPage
     ? "ui.main"
-    : useColorModeValue("ui.main", "teal.300")
+    : useColorModeValue("ui.main", "ui.main")
   const badgeBg = isLandingPage
     ? "ui.main"
-    : useColorModeValue("ui.main", "teal.300")
+    : useColorModeValue("ui.main", "teal")
   const badgeColor = isLandingPage
     ? "white"
-    : useColorModeValue("white", "gray.800")
+    : useColorModeValue("white", "white")
 
   return {
     bgColor,
