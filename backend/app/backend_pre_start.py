2bc129d9432eb8e301b90915b02988a8fc008353
Gustavo Carvalho <99373133+gustavosett@users.noreply.github.com>
2025-03-13 01:49:15 -0300

    chore: raise if not kubernetes initialized

--- backend/app/backend_pre_start.py
+++ backend/app/backend_pre_start.py
@@ -4,7 +4,9 @@ from sqlalchemy import Engine
 from sqlmodel import Session, select
 from tenacity import after_log, before_log, retry, stop_after_attempt, wait_fixed
 
+from app.core.config import settings
 from app.core.db import engine
+from app.integrations.kubernetes import kubernetes_manager
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -20,6 +22,9 @@ wait_seconds = 1
     after=after_log(logger, logging.WARN),
 )
 def init(db_engine: Engine) -> None:
+    if not kubernetes_manager.initialized and settings.ENVIRONMENT != "local":
+        raise Exception("Kubernetes client not initialized")
+
     try:
         # Setting join_transaction_mode to rollback_only prevents warnings when engine is already in transaction
         with Session(db_engine, join_transaction_mode="rollback_only") as session:
